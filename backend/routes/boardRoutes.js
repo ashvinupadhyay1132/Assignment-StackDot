1b3a0d14e244821a96a75cfd4b83c31f7244d4e4
Ashvin Upadhyay <64725443+ashvinupadhyay1132@users.noreply.github.com>
2025-05-21 06:51:38 +0530

    Delete backend directory

--- backend/routes/boardRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createBoard, getBoards } = require('../controllers/boardController');
-
-router.post('/', createBoard);
-router.get('/', getBoards);
-
-module.exports = router;
--- backend/routes/institueRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { registerInstitute, getInstitutes } = require('../controllers/instituteController');
-
-router.post('/register', registerInstitute);
-router.get('/', getInstitutes);
-
-module.exports = router;
